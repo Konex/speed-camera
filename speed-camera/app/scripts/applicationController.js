7f93dc6df367037fdcbeae50c275bde6f1c24e3f
YiniYin <yiniski@gmail.com>
2014-12-29 13:36:27 +1300

    refactors user auth out of app controller

--- speed-camera/app/scripts/applicationController.js
+++ speed-camera/app/scripts/applicationController.js
@@ -2,18 +2,72 @@
 
 var applicationController = angular.module('applicationController', []);
 
+
+
+var userPreferences = {};
+(function() {
+
+	var userSettings = {
+		maps:  {weatherOnOff:    false, trafficOnOff:      false, mobileCameraOnOff: false},
+		phone: {soundOnOff:      false, vibrationOnOff:    false},
+		user:  {myLocationOnOff: true,  needForSpeedOnOff: false, country: '', state: ''}
+	};
+
+	var $scope;
+
+	function init(_$scope) {
+		$scope = _$scope;
+	}
+
+	function getUserPreferences() {
+
+	}
+
+	userPreferences.init = init;
+})();
+
+
+var currentUser = {};
+(function() {
+	var $scope, USER_ROLES, AuthService;
+
+	function init(_$scope, _USER_ROLES, _AuthService) {
+		$scope      = _$scope;
+		USER_ROLES  = _USER_ROLES;
+		AuthService = _AuthService;
+
+		setDefaults();
+		setPublicMethods();
+	}
+
+	function setDefaults() {
+		$scope.currentUser  = null;
+		$scope.useRoles     = USER_ROLES;
+		$scope.isAuthorized = AuthService.isAuthorized;
+	}
+
+	function setPublicMethods() {
+		$scope.setCurrentUser = function(user) {
+			$scope.currentUser = user;
+		};
+	}
+
+	currentUser.init = init;
+})();
+
+
+
+
+
 applicationController.controller('ApplicationController', [
 	'$scope',
 	'USER_ROLES',
 	'AuthService',
 
   	function ($scope, USER_ROLES, AuthService) {
-		$scope.currentUser = null;
-		$scope.userRoles = USER_ROLES;
-		$scope.isAuthorized = AuthService.isAuthorized;
- 
-		$scope.setCurrentUser = function(user) {
-			$scope.currentUser = user;
-		};
+		// TODO: setup user auth if needed
+		//currentUser.init($scope, USER_ROLES, AuthService);
+
+		
 	}
 ]);
